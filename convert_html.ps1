f2e162341113a8b4d3bd904ab2c7b1396e3b8ef9
Davi Costa <davi.machado.costa@hotmail.com>
2021-06-07 10:01:34 -0300

    Create convert_html.ps1

---
+++ convert_html.ps1
@@ -0,0 +1,4 @@
+$OSinfo = Get-CimInstance -ClassName Win32_OperatingSystem | ConvertTo-Html -Property Version,Caption,BuildNumber,Manufacturer -Fragment
+$OSProcess = Get-CimInstance -ClassName Win32_Processor | ConvertTo-Html -Property DeviceId,Name,Caption,MaxClockSpeed,SocketDesignation - Fragment
+$Report = ConvertTo-HTML -Body "$OSinfo $OSProcess" -Title "Descritivo PC"
+$Report | Out-File ./OCS.html
