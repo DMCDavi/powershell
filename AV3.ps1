b559b2dca3c26032c200b325768ef8e17172847c
Davi Costa <davi.machado.costa@hotmail.com>
2021-06-07 12:27:21 -0300

    Getting services

--- AV3.ps1
+++ AV3.ps1
@@ -1,3 +1,14 @@
 function Get-PC-Name {
     Get-CimInstance -ClassName Win32_ComputerSystem | ConvertTo-HTML -Property Name -Fragment
 }
+
+function Get-Services {
+
+    param(
+        [string] $Status,
+        [int] $Quantity,
+        $Properties
+    )
+
+    Get-Service * | Where { $_.Status -eq $Status } | Select-Object -First $Quantity | ConvertTo-HTML -Property $Properties -Fragment
+}
